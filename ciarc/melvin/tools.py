50143720ff1bb8eda298917af2786734df4255d9
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2024-11-23 11:13:14 +0100

    Add CLI interactions

--- ciarc/melvin/tools.py
+++ ciarc/melvin/tools.py
@@ -1,12 +1,17 @@
 from ciarc.melvin.api import MelvinApi
 from typing import NoReturn
 from time import sleep
+from curses import window
 
 class ApiTools:
-  def __init__(self, api: MelvinApi):
+  def __init__(self, api: MelvinApi, screen: window):
     self.__api: MelvinApi = api
+    self.__screen: window = screen
   
   def telemetry_monitor(self) -> NoReturn:
     while True:
-      print(self.__api.get_telemetry())
+      self.__screen.clear()
+      telem_str = str(self.__api.get_telemetry())
+      self.__screen.addstr(telem_str)
+      self.__screen.refresh()
       sleep(0.5)
--- state_est/state_estimation.py
+++ state_est/state_estimation.py
@@ -1,13 +1,20 @@
 from ciarc.melvin.api import MelvinApi, build_uri
 from ciarc.melvin.tools import ApiTools
+from curses import wrapper
+from signal import signal, SIGINT
+from sys import exit
 
 ENDPOINT_IP = "10.100.50.1"
 ENDPOINT_PORT = 33000
 
-def main():
+def main(screen):
   api = MelvinApi(build_uri(ENDPOINT_IP, ENDPOINT_PORT))
-  tools = ApiTools(api)
+  tools = ApiTools(api, screen)
   tools.telemetry_monitor()
+  # Collect data
+
+  # Plot
 
 if __name__ == "__main__":
-  main()
+  signal(SIGINT, lambda sig, frame: exit(0))
+  wrapper(main)
