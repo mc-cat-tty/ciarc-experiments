9bde798bbb4a3e4090f0850ef8a2c68c9eef7987
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2024-11-23 13:54:18 +0100

    Introduce Telemetry and Energy defs; adapt from_telemetry accordingly

--- ciarc/melvin/defs.py
+++ ciarc/melvin/defs.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 from enum import Enum
 from dataclasses import dataclass, fields, asdict
 from operator import attrgetter
+from typing import Tuple
+from datetime import datetime
 
 class CameraAngle(Enum):
   NARROW = "narrow"
@@ -15,6 +17,44 @@ class MelvinState(Enum):
   COMMUNICATION = "communication"
   SAFE = "safe"
   TRANSITION = "transition"
+
+@dataclass(frozen=True)
+class Energy:
+  current_batt: float
+  max_batt: float
+  fuel: float
+
+  def get_battery_percentage(self):
+    return self.current_batt / self.max_batt
+
+@dataclass
+class Telemetry:
+  time: datetime
+  state: MelvinState
+  cam_angle: CameraAngle
+  energy: Energy
+  sim_speed: int
+  coord: Tuple[float, float]  # (x, y)
+  vel: Tuple[float]  # (vx, vy)
+
+  def __init__(
+    self,
+    state: str, angle: str, simulation_speed: int,
+    width_x: int, height_y: int, vx: float, vy: float,
+    battery: float, max_battery: float, fuel: float,
+    timestamp: str, *args, **kwargs
+  ):
+    self.time = datetime.fromisoformat(timestamp)
+    self.state = MelvinState(state)
+    self.cam_angle = CameraAngle(angle)
+    self.energy = Energy(battery, max_battery, fuel)
+    self.sim_speed = simulation_speed
+    self.coord = (width_x, height_y)
+    self.vel = (vx, vy)
+  
+  def __str__(self):
+    return f"x{self.sim_speed} [{self.state} {self.time:%d/%m/%Y %H:%M:%S}]: {self.coord=} {self.vel=} | {self.energy} | {self.cam_angle}"
+
 
 @dataclass(frozen=True)
 class StateControl:
@@ -37,30 +77,14 @@ class StateControl:
       return dict(_attributes)
 
     return asdict(self, dict_factory=factory)
-  
-  @staticmethod
-  def map_key(in_key: str) -> str:
-    match in_key:
-      case "angle": return "camera_angle"
-      case "vx": return "vel_x"
-      case "vy": return "vel_y"
-      case "state": return "state"
-      case _: return None
 
   @staticmethod
-  def from_telemetry(telemetry: dict) -> StateControl:
-    _telem = map(
-      lambda ele: (StateControl.map_key(ele[0]), ele[1]),
-      telemetry.items()
+  def from_telemetry(telemetry: Telemetry) -> StateControl:
+    return StateControl(
+      telemetry.vel[0],
+      telemetry.vel[1],
+      telemetry.cam_angle,
+      telemetry.state
     )
-
-    _telem = dict(
-      filter(
-        lambda k: k[0] is not None,
-        _telem
-      )
-    )
-
-    return StateControl(**_telem)
   
   
